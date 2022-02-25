94dfa375b26a513da3a07ae3435eee732b465d91
Alex Cole <jalexcole@gmail.com>
2022-02-25 21:25:27 +0000

    Added some structure

---
+++ src/CustomerList.cpp
@@ -0,0 +1,277 @@
+#include "CustomerList.hpp"
+#include "Store.h"
+
+#include <cstddef>
+
+/**
+ * @brief Construct a new Customer List:: Customer List object
+ *
+ */
+CustomerList::CustomerList() {
+  std::cout << "CustomerList::CustomerList()" << std::endl;
+  CustomerList::m_pHead = nullptr;
+}
+
+/**
+ * @brief Destroy the Customer List:: Customer List object
+ *
+ */
+CustomerList::~CustomerList() { m_pHead = nullptr; }
+
+/**
+ * @brief Get the Last object
+ *
+ * @param store
+ * @return Store*
+ */
+Store* getStoreTail(Store *store) {
+
+  std::cout << "Getting tail of store list" << std::endl;
+  if (store == nullptr) {
+    std::cout << "store is a nullptr" << std::endl;
+    return nullptr;
+  } else if (store->m_pNext == nullptr) {
+    std::cout << "Store is a single unit" << std::endl;
+    store->printStoreInfo();
+    return store;
+  } else {
+    std::cout << "Entering Recursion" << std::endl;
+    return getStoreTail(store->m_pNext);
+  }
+
+  // while (store->m_pNext != nullptr) {
+  //   store = store->m_pNext;
+  // }
+  // return store;
+}
+
+bool storeSingle(Store *store) {
+  if (store == nullptr) {
+    return false;
+  } else {
+    return store->m_pNext == nullptr;
+  }
+}
+
+int storeListSize(Store *store) {
+  // find the size of the store paramater;
+  int storeSize = 0;
+
+  if (store == nullptr) {
+    return false;
+  } else if (store->m_pNext == nullptr) {
+    storeSize++;
+  } else {
+    Store *temp = store;
+    while (temp->m_pNext != nullptr) {
+      storeSize++;
+      temp = temp->m_pNext;
+    }
+  }
+  return storeSize;
+}
+
+/**
+ * @brief Swaps Stores for sorting
+ *
+ * @param x
+ * @param y
+ */
+void swap(Store *x, Store *y) {
+  Store* temp = x;
+  x = y;
+  y = temp;
+}
+
+Store **storeListToArray(Store *store) {
+  int size = storeListSize(store);
+
+  Store *storeArray[size];
+  Store *temp = store;
+  for (int i = 0; i < size; i++) {
+    storeArray[i] = temp;
+    temp = temp->m_pNext;
+  }
+
+  return storeArray;
+}
+
+Store *storeArrayToList(Store **storeArray) {
+  int size = sizeof(storeArray) / sizeof(storeArray[0]);
+
+  Store *head = storeArray[0];
+  Store *temp = head;
+
+  for (int i = 1; i < size; i++) {
+    temp = temp->m_pNext;
+    temp = storeArray[i];
+  }
+
+  return head;
+}
+
+/**
+ * @brief Sorts the List of stores with a bubble sort
+ *
+ * @param store
+ * @return true
+ * @return false
+ */
+bool sortListByID(Store *store) {
+  std::cout << "CustomerList: Sorting list" << std::endl;
+  // Do nothing if the store is of size one
+  if (storeSingle(store)) {
+    return true;
+  }
+
+  bool swapped = false;
+  Store **storeArray = storeListToArray(store);
+  for (int i = 0; i < storeListSize(store) - 1; i++) {
+    swapped = false;
+    for (int j = 0; j < storeListSize(store) - i - j; j++) {
+      if (storeArray[j]->getStoreID() > storeArray[j + 1]->getStoreID()) {
+        swap(storeArray[j], storeArray[j + 1]);
+        swapped = true;
+      }
+    }
+    if (!swapped) {
+      store = storeArrayToList(storeArray);
+      return true;
+    }
+  }
+  return false;
+}
+
+Store *getLastStore(Store *store) {
+  Store *temp = store;
+
+  while (temp->m_pNext != nullptr) {
+    temp = temp->m_pNext;
+  }
+
+  return temp;
+}
+
+Store* sort(Store *store) {
+  
+  return nullptr;
+}
+
+/**
+ * @brief This is for the case that the store is empty and is not a list
+ * of its own.
+ *
+ * @details This assignment was probably expecting an interaction between two
+ * linked lists. That seemed to efficient. Therefore, Each linked list was
+ * converted to an array, a new array was created, with the contents of each
+ * array, followed by a bubble sort and then converted back to a linked list.
+ * @param store
+ * @return true
+ * @return false
+ */
+bool CustomerList::addStore(Store *store) {
+  if (store) {
+    if (m_pHead) {
+      getStoreTail(this->m_pHead)->m_pNext = store;
+      return true;
+    } else {
+      this->m_pHead = store;
+      return true;
+    }
+  } else {
+    return false;
+  }
+}
+
+/**
+ * @brief Removes a store from list based on id.
+ *
+ * @param ID
+ * @return Store*
+ */
+Store *CustomerList::removeStore(int ID) {
+  Store *temp = this->m_pHead;
+  Store *tempNext = this->m_pHead->m_pNext;
+
+  if (this->m_pHead->getStoreID() == ID) {
+    m_pHead = m_pHead->m_pNext;
+    return temp;
+  }
+
+  while (tempNext->m_pNext != NULL) {
+    if (tempNext->getStoreID() == ID) {
+      Store *continuation = tempNext->m_pNext;
+      temp->m_pNext = continuation;
+
+      return tempNext;
+    } else {
+      temp = tempNext;
+      tempNext = tempNext->m_pNext;
+    }
+  }
+
+  return nullptr;
+}
+
+/**
+ * @brief Returns a store with ID.
+ *
+ * @param ID
+ * @return Store*
+ */
+Store *CustomerList::getStore(int ID) {
+  if (m_pHead == nullptr) {
+    return nullptr;
+  }
+
+  Store* temp = m_pHead;
+
+  while (temp->m_pNext) {
+    if (temp->getStoreID() == ID) {
+      return temp;
+    } else {
+      temp = temp->m_pNext;
+    }
+  }
+  return nullptr;
+}
+
+/**
+ * @brief updates a store based on ID with selected information.
+ *
+ * @param ID
+ * @param name
+ * @param address
+ * @param city
+ * @param state
+ * @param zip
+ * @return true
+ * @return false
+ */
+bool CustomerList::updateStore(int ID, char *name, char *address, char *city,
+                               char *state, char *zip) {
+  Store *store = getStore(ID);
+  if (store == nullptr) {
+    return false;
+  }
+
+  // TODO: do some things to that store;
+  store->setStoreName(name);
+  store->setStoreCity(city);
+  store->setStoreState(state);
+  store->setStoreAddress(address);
+  store->setStoreZip(zip);
+  return false;
+}
+
+void CustomerList::printStoresInfo() {
+  std::cout << "Printing Store Info" << std::endl;
+  Store *temp = m_pHead;
+
+  while (temp != nullptr) {
+    temp->printStoreInfo();
+    temp = temp->m_pNext;
+  }
+
+  temp = nullptr;
+}
---
+++ src/EmployeeDatabase.cpp
@@ -0,0 +1,383 @@
+#include "EmployeeDatabase.hpp"
+
+#include <fstream>
+#include <iostream>
+#include <list>
+
+#include "CustomerList.hpp"
+#include "EmployeeRecord.hpp"
+
+/**
+ * @brief Construct a new Employee Database:: Employee Database object
+ *
+ */
+EmployeeDatabase::EmployeeDatabase() {}
+
+/**
+ * @brief Destroy the Employee Database:: Employee Database object
+ *
+ */
+EmployeeDatabase::~EmployeeDatabase() {}
+
+/**
+ * @brief Checks if the employee is not a binary tree;
+ *
+ * @param employee
+ * @return true
+ * @return false
+ */
+bool single(EmployeeRecord *employee) {
+  return employee->m_pLeft == nullptr && employee->m_pRight == nullptr;
+}
+
+/**
+ * @brief Converts and EmployeeRecord binary tree to a list to iterator through
+ *
+ * @param employee
+ * @return std::list<EmployeeRecord*>*
+ */
+std::list<EmployeeRecord *> *employeeToList(EmployeeRecord *employee) {
+  std::list<EmployeeRecord *> *list = new std::list<EmployeeRecord *>();
+  EmployeeRecord *root = employee;
+
+  std::list<EmployeeRecord *> *temp = new std::list<EmployeeRecord *>();
+  std::list<EmployeeRecord *> *tempNext = new std::list<EmployeeRecord *>();
+
+  temp->push_back(root);
+  while (temp->size() != 0) {
+    while (temp->size() != 0) {
+      list->push_back(temp->front());
+      if (temp->front()->m_pRight != nullptr) {
+        tempNext->push_back(temp->front()->m_pRight);
+      }
+
+      if (temp->front()->m_pLeft != nullptr) {
+        tempNext->push_back(temp->front()->m_pLeft);
+      }
+
+      temp->pop_front();
+    }
+    delete temp;
+    temp = tempNext;
+    tempNext = new std::list<EmployeeRecord *>();
+  }
+
+  // release resources
+  if (temp != nullptr) {
+    delete temp;
+  }
+
+  if (tempNext != nullptr) {
+    delete tempNext;
+  }
+
+  return list;
+}
+
+/**
+ * @brief inserts the target EmployeeRecord into the source tree
+ *
+ * @description Recursivly goes though each node on a binary tree for
+ * comparison.
+ * @param source
+ * @param target  Assumes a single employee.
+ * @return true
+ * @return false
+ */
+bool insertEmployee(EmployeeRecord *source, EmployeeRecord *target) {
+  if (source == nullptr || target == nullptr) {
+    return false;
+  }
+
+  if (source->getID() < target->getID()) {
+    // add left;
+    if (source->m_pLeft != nullptr) {
+      insertEmployee(source->m_pLeft, target);
+    } else {
+      source->m_pLeft = target;
+      return true;
+    }
+  } else {
+    if (source->m_pRight != nullptr) {
+      insertEmployee(source->m_pRight, target);
+    } else {
+      source->m_pRight = target;
+      return true;
+    }
+  }
+  return false;
+}
+
+/**
+ * @brief Adds an employee or Employee Record tree to the Employee Record Tree.
+ *
+ * @param employee  Can be a single employee or a tree of employees.
+ * @return true
+ * @return false
+ */
+bool EmployeeDatabase::addEmployee(EmployeeRecord *employee) {
+  // Determine if the employeee is a single employee
+
+  if (single(employee)) {
+    // add to tree;
+    return insertEmployee(&m_pRoot, employee);
+
+  } else {
+    // parse into list and then add back into tree;
+    std::list<EmployeeRecord *> *employees = employeeToList(employee);
+
+    while (!employees->empty()) {
+      // traverse tree to add each employee;
+      EmployeeRecord *employeefromList = employees->front();
+
+      employeefromList->m_pLeft = nullptr;
+      employeefromList->m_pRight = nullptr;
+
+      insertEmployee(&m_pRoot, employeefromList);
+
+      employees->pop_front();
+    }
+    delete employees;
+  }
+  return false;
+}
+
+/**
+ * @brief Finds an employee Record by from a given Tree by id.
+ *
+ * @param employee
+ * @param id
+ * @return EmployeeRecord*
+ */
+EmployeeRecord *findEmployeeById(EmployeeRecord *employee, int id) {
+  if (employee->getID() == id) {
+    return employee;
+  } else if (employee->getID() < id) {
+    if (employee->m_pLeft == nullptr) {
+      return nullptr;
+    }
+    return findEmployeeById(employee->m_pLeft, id);
+  } else {
+    if (employee->m_pRight == nullptr) {
+      return nullptr;
+    }
+    return findEmployeeById(employee->m_pRight, id);
+  }
+}
+
+/**
+ * @brief Gets the Employee with the given id.
+ *
+ * @param ID
+ * @return EmployeeRecord*
+ */
+EmployeeRecord *EmployeeDatabase::getEmployee(int ID) {
+  return findEmployeeById(&m_pRoot, ID);
+}
+
+/**
+ * @brief Removes an Employee from the database.
+ *
+ * @param ID
+ * @return EmployeeRecord*
+ */
+EmployeeRecord *EmployeeDatabase::removeEmployee(int ID) {
+  // find the employee
+  EmployeeRecord *employee = findEmployeeById(&m_pRoot, ID);
+  if (employee == nullptr) {
+    return nullptr;
+  }
+  // index childeren of employee
+  EmployeeRecord *left = employee->m_pLeft;
+  EmployeeRecord *right = employee->m_pRight;
+
+  // find parent node of employee;
+  EmployeeRecord *parent = &m_pRoot;
+
+  bool found = false;
+
+  while (!found) {
+    if (parent->m_pLeft == employee) {
+      found = true;
+      break;
+    } else if (parent->m_pRight == employee) {
+      found = true;
+      break;
+    }
+
+    if (parent->getID() > ID) {
+      parent = parent->m_pLeft;
+    } else {
+      parent = parent->m_pRight;
+    }
+  }
+
+  if (parent->m_pRight == employee) {
+    parent->m_pRight = nullptr;
+    if (left != nullptr) {
+      addEmployee(left);
+    }
+    if (right != nullptr) {
+      addEmployee(right);
+    }
+  }
+  return employee;
+}
+
+void EmployeeDatabase::printEmployeeDatabase() {
+  std::list<EmployeeRecord *> *employeeDatabase = employeeToList(&m_pRoot);
+
+  while (!employeeDatabase->empty()) {
+    employeeDatabase->front()->printRecord();
+    employeeDatabase->pop_front();
+  }
+}
+
+/**
+ * @brief Prints out an employee record tree.
+ *
+ * @param record
+ */
+void EmployeeDatabase::printEmployeeRecords(EmployeeRecord *record) {
+
+  // Allacate employees
+  std::list<EmployeeRecord *> *employees = employeeToList(record);
+
+  while (!employees->empty()) {
+    EmployeeRecord *employeeFromList = employees->front();
+    employeeFromList->printRecord();
+    employees->pop_front();
+  }
+
+  delete employees; // deallicate employees
+}
+
+//-----------------------------------------------------
+// Build the database
+//-----------------------------------------------------
+bool EmployeeDatabase::buildDatabase(const char *dataFile) {
+  std::cout << "Test: Starting To Build Databas" << std::endl;
+  bool OK = true;
+  int numEmp, id, dept, numStores, sID;
+  double sal;
+  EmployeeRecord *empRec;
+  class CustomerList *theList;
+  Store *theStore;
+  char inStr[128];
+  char fName[32];
+  char lName[32];
+  char sName[64];
+  char sAddr[64];
+  char sSt[32];
+  char sCity[32];
+  char sZip[12];
+
+  std::cout << "Test: Attempting to Open File" << std::endl;
+  inFile.open(dataFile, ifstream::in);
+  if (!inFile.is_open()) {
+    // inFile.is_open() returns false if the file could not be found or
+    //    if for some other reason the open failed.
+    cout << "Unable to open file" << dataFile << "\nProgram terminating...\n";
+    cout << "Press Enter to continue...";
+    getc(stdin);
+    return false;
+  }
+
+
+  // Get number of employees
+  getNextLine(inStr, 128);
+  numEmp = atoi(inStr);
+  for (int i = 0; i < numEmp; i++) {
+    // Instantiate an EmployeeRecord
+    empRec = new EmployeeRecord();
+    // Read and set the ID
+    getNextLine(inStr, 127);
+    id = atoi(inStr);
+    empRec->setID(id);
+    // Read and set the name
+    getNextLine(fName, 31);
+    getNextLine(lName, 31);
+    empRec->setName(fName, lName);
+    // Read and set the Department ID
+    getNextLine(inStr, 127);
+    dept = atoi(inStr);
+    empRec->setDept(dept);
+    // Read and set the Salary
+    getNextLine(inStr, 127);
+    sal = atof(inStr);
+    empRec->setSalary(sal);
+    // Get the customer list
+    theList = empRec->getCustomerList();
+    // Get the number of stores
+    getNextLine(inStr, 127);
+    numStores = atoi(inStr);
+    for (int j = 0; j < numStores; j++) {
+      // Read the store ID
+      getNextLine(inStr, 127);
+      sID = atoi(inStr);
+      // Read the store name
+      getNextLine(sName, 63);
+      // Read the store address
+      getNextLine(sAddr, 63);
+      // Read the store city
+      getNextLine(sCity, 31);
+      // Read the store state
+      getNextLine(sSt, 31);
+      // Read the store zip
+      getNextLine(sZip, 11);
+      // Create a new Store object
+      std::cout << "Test: Generating Stores :" << std::endl; // for debugging
+      theStore = new Store(sID, sName, sAddr, sCity, sSt, sZip);
+      
+      std::cout << "EmployeeDatabase::buildDataBase() adding new Store to list" << std::endl;
+      theStore->printStoreInfo();
+      theList->addStore(theStore);
+
+      std::cout << "EmoloyeeDatabase::buildDatabase() printing store info" << std::endl;
+      theList->printStoresInfo(); // For Debugging
+    }
+    cout.flush();
+    std::cout << "Adding Employee to Employee database" << std::endl;
+    addEmployee(empRec);
+  }
+  inFile.close();
+  return true; // Successfully built the database
+}
+
+//--------------------------------------------
+// GetNextLine()
+// Read a line from a data file.
+// Author: Rick Coleman
+// Used by permission
+//--------------------------------------------
+bool EmployeeDatabase::getNextLine(char *line, int lineLen) {
+  std::cout << "Test: Getting Next Line" << std::endl;
+  int done = false;
+  while (!done) {
+    inFile.getline(line, 128);
+
+    if (inFile.good()) // If a line was successfully read
+    {
+      if (strlen(line) == 0) // Skip any blank lines
+        continue;
+      else if (line[0] == '#') // Skip any comment lines
+        continue;
+      else
+        return true; // Got a valid data line so return with this line
+    } else {
+      strcpy(line, "");
+      return false;
+    }
+  } // end while
+  return false;
+}
+
+void EmployeeDatabase::destroyTree(EmployeeRecord* rt) {
+  std::list<EmployeeRecord*>* list = employeeToList(rt);
+  while (!list->empty()) {
+    EmployeeRecord* record = list->front();
+    list->pop_front();
+    delete record;
+  }
+  delete list;
+}
